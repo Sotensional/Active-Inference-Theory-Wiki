75f05c5aace0f0ce65f8daf122df6cc20dd4eecb
Sotensional <koljakurzer@gmail.com>
2025-03-11 12:45:02 +0100

    Quartz sync: Mar 11, 2025, 12:45 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -17,8 +17,6 @@ export const sharedPageComponents: SharedLayout = {
       category: 'Announcements',
       // from data-category-id
       categoryId: 'DIC_kwDOOGsX-84Cn08l',
-	    //theme
-	    data-theme: "noborder_light",
     }
   }),
 ],
