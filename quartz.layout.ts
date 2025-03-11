bff4f1427c01d7f230d27274402e8cd100b5c36f
Sotensional <koljakurzer@gmail.com>
2025-03-11 13:04:49 +0100

    Quartz sync: Mar 11, 2025, 1:04 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -5,7 +5,21 @@ import * as Component from "./quartz/components"
 export const sharedPageComponents: SharedLayout = {
   head: Component.Head(),
   header: [],
-  afterBody: [],
+  afterBody: [
+  Component.Comments({
+    provider: 'giscus',
+    options: {
+      // from data-repo
+      repo: 'sotensional/Active-Inference-Theory-Wiki',
+      // from data-repo-id
+      repoId: 'R_kgDOOGsX-w',
+      // from data-category
+      category: 'Announcements',
+      // from data-category-id
+      categoryId: 'DIC_kwDOOGsX-84Cn08l',
+    }
+  }),
+],
   footer: Component.Footer({
     links: {
       GitHub: "https://github.com/jackyzha0/quartz",
