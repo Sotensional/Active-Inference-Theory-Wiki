0c8526debf31e7ff4add634f2b9f468ef5823aa7
Sotensional <koljakurzer@gmail.com>
2025-03-11 12:47:44 +0100

    Quartz sync: Mar 11, 2025, 12:47 PM

--- quartz/components/Head.tsx
+++ quartz/components/Head.tsx
@@ -210,7 +210,7 @@ export default (() => {
         )}
         <link rel="icon" href={iconPath} />
         <meta name="description" content={description} />
-        <meta name="generator" content="Active Inference Wiki" />
+        <meta name="generator" content="Quartz" />
         {css.map((resource) => CSSResourceToStyleElement(resource, true))}
         {js
           .filter((resource) => resource.loadTime === "beforeDOMReady")
